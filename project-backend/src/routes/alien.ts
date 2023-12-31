f823c834845ec326a31f5e6e42d84da470f44214
Andrew Tran <andrewtranva@gmail.com>
2023-12-30 21:05:41 -0500

    Added routes interacting the backend with the MongoDB database instance (got help from Pierre) but it finally works

---
+++ project-backend/src/routes/alien.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from "express";
+// import { Alien } from "../../assets/types";
+import { AlienModel, AlienSchema } from "../models/alien";
+import log from "../utils/logger";
+
+const router = express.Router();
+
+router.get("/api/alien", async (req: Request, res: Response) => {
+    await AlienModel.find()
+        .then((items) => {
+            items.forEach((item, index) => {
+                log.info(`Item ${index}`);
+                log.info(item);
+            });
+            log.info("Successfully found at least one alien!");
+            return res.status(200).json(items);
+        })
+        .catch((err) => {
+            log.error("Did not find any aliens! Please try again!");
+            return res.status(400).send({ "Error-GET": err });
+        });
+});
+
+router.post("/api/alien", async (req: Request, res: Response) => {
+    const alienToInsert = new AlienModel({
+        name: req.body.name,
+        tech: req.body.tech,
+        age: req.body.age,
+        sub: req.body.sub
+    });
+
+    try {
+        const alienResult = await alienToInsert.save();
+        log.info("Added alien successfully! Congratulations!");
+        return res.status(200).json(alienResult);
+    } catch (err) {
+        log.error({ "Error-POST": err });
+        log.error("Could not add alien! Please try again!");
+        return res.status(400).send({ "Error-POST": err });
+    }
+});
+
+export { router as alienRouter };
--- project-backend/src/routes/example.ts
+++ project-backend/src/routes/example.ts
@@ -1,13 +1,13 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
 
 router.get("/api/example", (req: Request, res: Response) => {
-    return res.status(200).send("Example GET request at /api/example");
+    return res.status(200).send({ data: "Example GET request at /api/example\r\n" });
 });
 
-// router.post("/api/example", (req: Request, res: Response) => {
-//     return res.send({ "data": "NEW example created!" });
-// });
+router.post("/api/example", (req: Request, res: Response) => {
+    return res.status(201).send({ data: "NEW example created!\r\n" });
+});
 
 export { router as exampleRouter };
