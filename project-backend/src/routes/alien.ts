e8a454b94ba5cc9e7aff59268e2589329325633e
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:33:54 -0500

    Fixed GET express endpoint, Added POST (insert), PUT (update), and DELETE (delete) endpoints in Express successfully, and Got rid of extra/redundant Request/Response TS type annotations

--- project-backend/src/routes/alien.ts
+++ project-backend/src/routes/alien.ts
@@ -1,26 +1,33 @@
 import express, { Request, Response } from "express";
 import { AlienModel, AlienSchema } from "../models/alien";
 import log from "../utils/logger";
+import { Alien } from "../../assets/types";
 
 const router = express.Router();
 
-router.get("/api/alien", async (req: Request, res: Response) => {
-    await AlienModel.find()
-        .then((items) => {
-            items.forEach((item, index) => {
-                log.info(`Item ${index}`);
-                log.info(item);
-            });
-            log.info("Successfully found at least one alien!");
-            return res.status(200).json(items);
-        })
-        .catch((err) => {
-            log.error("Did not find any aliens! Please try again!");
-            return res.status(400).send({ "Error-GET": err });
+// Reference (HTTP Methods): https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+// Reference: https://jasonwatmore.com/post/2021/09/21/fetch-http-delete-request-examples
+let index = 0;
+router.get("/api/alien", async (_, res) => {
+    try {
+        const currItems = await AlienModel.find();
+
+        currItems.forEach((item, idx) => {
+            log.info(`Item ${idx}`);
+            log.info(item);
         });
+
+        log.info("Successfully found at least one alien!");
+        log.info(`END OF GET REQUEST #${index} ------------------------`);
+        index++;
+        return res.status(200).json(currItems);
+    } catch (err: any) {
+        log.error("Did not find any aliens! Please try again!");
+        return res.status(400).send({ Error_GET: err.message });
+    }
 });
 
-router.post("/api/alien", async (req: Request, res: Response) => {
+router.post("/api/alien", async (req, res) => {
     const alienToInsert = new AlienModel({
         name: req.body.name,
         tech: req.body.tech,
@@ -29,13 +36,49 @@ router.post("/api/alien", async (req: Request, res: Response) => {
     });
 
     try {
-        const alienResult = await alienToInsert.save();
-        log.info("Added alien successfully! Congratulations!");
-        return res.status(200).json(alienResult);
-    } catch (err) {
-        log.error({ "Error-POST": err });
-        log.error("Could not add alien! Please try again!");
-        return res.status(400).send({ "Error-POST": err });
+        const alienInsertResult = await alienToInsert.save();
+        log.info("Inserted alien successfully! Congratulations!");
+        return res.status(201).json(alienInsertResult); // 201 = Successful Resource Creation
+    } catch (err: any) {
+        log.error({ Error_POST: err });
+        log.error("Could not insert the specified alien! Please try again!");
+        return res.status(400).send({ Error_POST: err.message });
+    }
+});
+
+router.put("/api/alien", async (req, res) => {
+    const alienToUpdateId = req.body._id;
+    const alienToUpdateFields: Alien = {
+        name: req.body.name,
+        tech: req.body.tech,
+        age: req.body.age,
+        sub: req.body.sub
+    };
+
+    try {
+        const alienUpdateResult = await AlienModel.findByIdAndUpdate(alienToUpdateId, alienToUpdateFields, {
+            new: true
+        });
+        log.info("Updated alien successfully! Congratulations!");
+        return res.status(200).json(alienUpdateResult);
+    } catch (err: any) {
+        log.error({ Error_PUT: err });
+        log.error("Could not update the specified alien! Please try again!");
+        return res.status(400).send({ Error_PUT: err.message });
+    }
+});
+
+router.delete("/api/alien", async (req, res) => {
+    const alienToDeleteId = req.body._id;
+
+    try {
+        const alienDeleteResult = await AlienModel.findByIdAndDelete(alienToDeleteId);
+        log.info("Deleted alien successfully! Congratulations!");
+        return res.status(200).json(alienDeleteResult);
+    } catch (err: any) {
+        log.error({ Error_DELETE: err });
+        log.error("Could not delete the specified alien! Please try again!");
+        return res.status(400).send({ Error_DELETE: err.message });
     }
 });
 
