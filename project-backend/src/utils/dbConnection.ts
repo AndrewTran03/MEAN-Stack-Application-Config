33fb5a53efd72ca377e1433f85664381e11b24f6
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:34:32 -0500

    Removed unnecessary comments and code snippets

--- project-backend/src/utils/dbConnection.ts
+++ project-backend/src/utils/dbConnection.ts
@@ -1,19 +1,12 @@
-// import { MongoClient, ServerApiVersion } from "mongodb";
 import mongoose from "mongoose";
 import config from "config";
 import log from "./logger";
 
 async function ensureConnectionToMongoDatabase() {
     const dbUri = config.get<string>("mongoDatabaseUri");
-    mongoose.Promise = global.Promise;
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await mongoose.connect(dbUri);
         log.info("Sucessfully initiated connection to MongoDB");
-
-        mongoose.connection.once("open", () => {
-            log.info("MongoDB operation successful");
-        });
     } catch (e) {
         log.error("Failed to connect to MongoDB. Exiting now...");
         process.exit(1);
