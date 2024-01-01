9823952e334c8884827b6b5877f1681d15c1abdc
Andrew Tran <andrewtranva@gmail.com>
2024-01-01 16:55:05 -0500

    Edited script echo/print statementfrom React to Angular

--- project-frontend/run_frontend.sh
+++ project-frontend/run_frontend.sh
@@ -2,6 +2,6 @@
 echo "|-------- Beginning of Running Front-End Script --------|"
 echo "Installing npm Dependencies"
 time npm install
-echo "Running React Frontend:"
+echo "Running Angular Frontend:"
 npm run start
 echo "|-------- End of Running Front-End Script --------|"
