324c4fad0bda898e00c04ac09edadb8246d53844
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:36:58 -0500

    Updated 'dotenv' import to modern import ES6+ syntax, Removed extra TS annotation for Request in code, and properly set specifc headers for 'Access-Control-Allow-Origin' and 'Access-Control-Allow-Methods'

--- project-backend/src/background_app.ts
+++ project-backend/src/background_app.ts
@@ -1,5 +1,7 @@
-require("dotenv").config();
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Response } from "express";
 import bodyParser from "body-parser";
 import config from "config";
 import cors from "cors";
@@ -7,13 +9,16 @@ import log from "./utils/logger";
 import router from "./routes";
 import { ensureConnectionToMongoDatabase } from "./utils/dbConnection";
 
-// Link: https://medium.com/swlh/typescript-with-mongoose-and-node-express-24073d51d2ee
+// Link: https://medium.com/swlh/typescript-with-mongoose-and-node-express-24073d51d2eed
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({ credentials: true }));
-
 app.use(router);
+app.use((_, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
+});
 
 const port = config.get<number>("port");
 
