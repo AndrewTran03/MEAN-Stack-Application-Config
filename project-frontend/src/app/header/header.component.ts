790685a3f6c2dae1f8168f46324e2015a7a2c349
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:47:14 -0500

    Added integration of the backend to the frontend

--- project-frontend/src/app/header/header.component.ts
+++ project-frontend/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, Injector, computed, effect, signal } from "@angular/core";
-import { User } from "../shared/models";
+import { Component, computed, effect, signal, OnInit, OnDestroy } from "@angular/core";
+import { Generic_User, Alien } from "../shared/models";
+import { AlienService } from "../services/alien.service";
 
 @Component({
     selector: "app-header",
@@ -8,26 +9,50 @@ import { User } from "../shared/models";
     templateUrl: "./header.component.html",
     styleUrl: "./header.component.css"
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+    private refreshInterval: any;
+    readonly MAX_TIMER_INTERVAL = 5000;
+
+    aliens = signal<Alien[]>([]);
+
     counter = signal(0);
-    readonly counterEffect = effect(
-        () => {
-            console.log(`Counter is ${this.counter()}`);
-        },
-        { injector: this.injector }
-    );
     readonly computedVal = computed(() => this.counter() + 10);
-    numbers = signal<User[]>([
+
+    numbers = signal<Generic_User[]>([
         { id: 1, fName: "Andrew" },
         { id: 2, fName: "Anderson" },
         { id: 3, fName: "Emily " }
     ]);
 
-    constructor(private injector: Injector) {}
+    constructor(private alienService: AlienService) {
+        effect(() => {
+            console.log(`Counter is ${this.counter()}`);
+        });
+        effect(() => {
+            console.log(this.aliens());
+        });
+    }
+
+    ngOnInit() {
+        this.refresh();
+    }
+
+    ngOnDestroy() {
+        clearInterval(this.refreshInterval);
+    }
+
+    refresh() {
+        this.getAlienData();
+        if (this.refreshInterval) {
+            clearInterval(this.refreshInterval);
+            this.refreshInterval = setInterval(() => {
+                this.getAlienData();
+            }, this.MAX_TIMER_INTERVAL);
+        }
+    }
 
     incrementCounter() {
         this.counter.update((prev) => prev + 1);
-        // console.clear();
     }
 
     decrementCounter() {
@@ -38,11 +63,15 @@ export class HeaderComponent {
                 console.log("New value is negative. Please do not go further negative!");
             }
 
-            return newVal < 0 ? 0 : newVal;
+            return newVal <= 0 ? 0 : newVal;
         });
     }
 
     setStaticDefaultCounter() {
         this.counter.set(0);
+    }
+
+    getAlienData() {
+        this.alienService.getAliens().then((data) => this.aliens.set(data));
     }
 }
