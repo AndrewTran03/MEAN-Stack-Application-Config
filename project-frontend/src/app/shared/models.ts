2b195ea342e863bd12f9d466fee6f2535b2d7a53
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:43:53 -0500

    Added new shared types - Alien and those found in most MongoDB entries (called MongoDBEntry), Wrote a common enum for accessing common HTTP request methods, and Prettify (coding-ease for seeing types), and also the exportable link to the backend API built with Node.js/Express.js via the localhost URL

--- project-frontend/src/app/shared/models.ts
+++ project-frontend/src/app/shared/models.ts
@@ -1,7 +1,41 @@
+// Shared Typescript Types/Interfaces/Other Global-Variables Used Throughout the Project:
 
-type User = {
-    id: number,
-    fName: string
+// Reference: https://www.totaltypescript.com/concepts/the-prettify-helper
+type Prettify<T> = {
+    [K in keyof T]: T[K];
+} & {};
+
+// Example
+type Generic_User = {
+    id: number;
+    fName: string;
 };
 
-export { User };
+// Required with All MongoDB Entries:
+type MongoDBEntry = {
+    _id: string; // Primary Key (IDentifier)
+    __v: number; // Version Number (Auto-Increments - Avoiding Duplicate Entry-Modification)
+};
+
+const aliensBackendUrlBase = "http://localhost:3000/api/alien";
+
+type Alien = Prettify<
+    MongoDBEntry & {
+        name: string;
+        tech: string;
+        age: number;
+        sub: boolean;
+    }
+>;
+
+enum HTTP_REQUEST_METHODS {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    OPTIONS,
+    HEAD
+}
+
+export { Generic_User, Alien, aliensBackendUrlBase, HTTP_REQUEST_METHODS };
