cede69aa2a2c21e3fdc2539b85880f0f7acf0a7b
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:47:57 -0500

    Removed unnecessary module file in the 'shared' directory

--- project-frontend/src/app/shared/shared.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { User } from "./models";
-
-@NgModule({
-    declarations: [],
-    imports: [CommonModule]
-})
-export class SharedModule {}
