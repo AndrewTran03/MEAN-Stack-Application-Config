a06ba5e5f0db4340399ef924bc4e64edc80852ea
Andrew Tran <andrewtranva@gmail.com>
2023-12-31 21:32:44 -0500

    Got rid of extra/redundant Request/Response TS type annotations

--- project-backend/src/routes/example.ts
+++ project-backend/src/routes/example.ts
@@ -2,11 +2,11 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.get("/api/example", (req: Request, res: Response) => {
+router.get("/api/example", (req, res) => {
     return res.status(200).send({ data: "Example GET request at /api/example\r\n" });
 });
 
-router.post("/api/example", (req: Request, res: Response) => {
+router.post("/api/example", (req, res) => {
     return res.status(201).send({ data: "NEW example created!\r\n" });
 });
 
