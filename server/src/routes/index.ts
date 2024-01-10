ade91845eb28e77f8facb112b4dec92041cd59db
Andrew Tran <andrewtranva@gmail.com>
2024-01-10 17:03:00 -0500

    Added the Human API routes to the global backend Express router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import { exampleRouter } from "./example";
 import { alienRouter } from "./alien";
 import { userRouter } from "./authentication";
+import { humanRouter } from "./human";
 
 const router = express.Router();
 
 router.use(exampleRouter);
 router.use(alienRouter);
 router.use(userRouter);
+router.use(humanRouter);
 
 export default router;
