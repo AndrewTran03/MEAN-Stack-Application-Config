4d24e2721dd729856a417c890989eadfb1575d76
Andrew Tran <andrewtranva@gmail.com>
2024-01-10 17:30:57 -0500

    Renamed 'numbers' variable to 'users' in TS file and changed Header Component of AlienService from constructor injection to use the modern syntax of the inject()

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -1,5 +1,5 @@
-import { Component, computed, effect, signal, OnInit, OnDestroy } from "@angular/core";
-import { Generic_User, Alien } from "../shared/types";
+import { Component, computed, effect, signal, OnInit, OnDestroy, inject } from "@angular/core";
+import { GenericUser, Alien, AlienBase, Prettify } from "../shared/types";
 import { AlienService } from "../services/alien.service";
 
 @Component({
@@ -10,6 +10,7 @@ import { AlienService } from "../services/alien.service";
     styleUrl: "./header.component.css"
 })
 export class HeaderComponent implements OnInit, OnDestroy {
+    private alienService = inject(AlienService);
     private refreshInterval: any;
     readonly MAX_TIMER_INTERVAL = 5000;
 
@@ -18,13 +19,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
     counter = signal(0);
     readonly computedVal = computed(() => this.counter() + 10);
 
-    numbers = signal<Generic_User[]>([
+    users = signal<GenericUser[]>([
         { id: 1, fName: "Andrew" },
         { id: 2, fName: "Anderson" },
         { id: 3, fName: "Emily " }
     ]);
 
-    constructor(private alienService: AlienService) {
+    constructor() {
         effect(() => {
             console.log(`Counter is ${this.counter()}`);
         });
