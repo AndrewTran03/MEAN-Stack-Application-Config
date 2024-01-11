f8db8b52c6e7f3950e06847524c6605f02e449c0
Andrew Tran <andrewtranva@gmail.com>
2024-01-11 13:20:05 -0500

    Removed depreciated argument 'useNewUrlParser' from mongoose.connect() async function call

--- server/src/utils/mongoConnection.ts
+++ server/src/utils/mongoConnection.ts
@@ -8,7 +8,7 @@ async function ensureConnectionToMongoDatabase() {
 
     try {
         log.info(dbUri);
-        await mongoose.connect(dbUri, { useNewUrlParser: true });
+        await mongoose.connect(dbUri);
         log.info("Sucessfully initiated connection to MongoDB");
     } catch (e) {
         log.error("Failed to connect to MongoDB. Exiting now...");
