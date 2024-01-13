3530be053e87739d8d18a6cad02ab12edeae90e2
Andrew Tran <andrewtranva@gmail.com>
2024-01-13 10:56:59 -0500

    Modified frontend script to include formatting

--- client/run_frontend.sh
+++ client/run_frontend.sh
@@ -4,5 +4,6 @@ echo "|-------- Beginning of Running Front-End Script --------|"
 echo "Installing NPM Dependencies"
 time npm install
 echo "Running Angular Frontend:"
+npm run format
 npm run start
 echo "|-------- End of Running Front-End Script --------|"
