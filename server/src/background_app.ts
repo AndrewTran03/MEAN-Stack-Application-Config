a95b377884bd3334f2179f12a9a8fbeb975a7fce
Andrew Tran <andrewtranva@gmail.com>
2024-01-15 19:18:40 -0500

    Added the 'Accesss-Control-Request-Method' header

--- server/src/background_app.ts
+++ server/src/background_app.ts
@@ -21,6 +21,7 @@ app.use(cors({ credentials: true }));
 app.use(router);
 app.use((_, res, next: NextFunction) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Request-Method", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
